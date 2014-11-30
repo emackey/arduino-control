d536f64d79a4804a191cfe60ab715673a1cb9da7
Ed Mackey <elm19087@gmail.com>
2014-11-30 16:51:08 -0500

    First draft of the port and pin collections.

---
+++ VisualStudio/ArduinoControl/ArduinoPin.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoControl
+{
+    public enum ArduinoPinMode
+    {
+        AnalogIn, AnalogOut, DigitalIn, DigitalOut
+    }
+
+    public class ArduinoPin
+    {
+        private ArduinoPort m_port;
+        private int m_value;
+
+        public string Name { get; private set; }
+        public int Number { get; private set; }
+        public ArduinoPinMode Mode { get; private set; }
+
+        public ArduinoPin(ArduinoPort port, string pinDescription)
+        {
+            m_port = port;
+
+            string modeString = pinDescription.Substring(0, 2);
+            switch (modeString)
+            {
+                case "AI":
+                    Mode = ArduinoPinMode.AnalogIn;
+                    m_value = 0;
+                    break;
+                case "AO":
+                    Mode = ArduinoPinMode.AnalogOut;
+                    m_value = 255;
+                    break;
+                case "DI":
+                    Mode = ArduinoPinMode.DigitalIn;
+                    m_value = 0;
+                    break;
+                case "DO":
+                    Mode = ArduinoPinMode.DigitalOut;
+                    m_value = 1;
+                    break;
+                default:
+                    throw new Exception("Unknown pin mode");
+            }
+
+            int pos = pinDescription.IndexOf('"');
+            if (pos < 2)
+            {
+                throw new Exception("Missing pin name or number");
+            }
+
+            Number = int.Parse(pinDescription.Substring(2, pos - 2));
+
+            int pos2 = pinDescription.LastIndexOf('"');
+            Name = pinDescription.Substring(pos + 1, pos2 - pos);
+        }
+
+        public void Disconnect()
+        {
+            m_port = null;
+        }
+
+        public int Value
+        {
+            get
+            {
+                return m_value;
+            }
+            set
+            {
+                switch (Mode)
+                {
+                    case ArduinoPinMode.AnalogIn:
+                        throw new Exception("Can't write to an analog input pin.");
+
+                    case ArduinoPinMode.AnalogOut:
+                        if ((value < 0) || (value > 255))
+                        {
+                            throw new Exception("Analog values must be between 0 and 255, inclusive.");
+                        }
+                        m_port.Write("A" + Number + ":" + value + ";");
+                        break;
+
+                    case ArduinoPinMode.DigitalIn:
+                        throw new Exception("Can't write to a digital input pin.");
+
+                    case ArduinoPinMode.DigitalOut:
+                        if ((value < 0) || (value > 1))
+                        {
+                            throw new Exception("Digital values must be 0 or 1.");
+                        }
+                        m_port.Write("D" + Number + ":" + value + ";");
+                        break;
+                }
+                m_value = value;
+            }
+        }
+    }
+}
---
+++ VisualStudio/ArduinoControl/ArduinoPort.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoControl
+{
+    public class ArduinoPort : SerialPort
+    {
+        private List<ArduinoPin> m_pins;
+        private string m_receivedBytes;
+
+        public event EventHandler PinsAvailable;
+
+        public ArduinoPort(string portName, int baudRate)
+            : base(portName, baudRate)
+        {
+            m_pins = new List<ArduinoPin>();
+            m_receivedBytes = String.Empty;
+
+            DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            Open();
+            Write("@LIST;");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            foreach (var pin in m_pins)
+            {
+                pin.Disconnect();
+            }
+            m_pins.Clear();
+            base.Dispose(disposing);
+        }
+
+        protected void OnDataReceived(string data)
+        {
+            m_receivedBytes += data;
+
+            int pos;
+            while ((pos = m_receivedBytes.IndexOf(';')) >= 0)
+            {
+                string command = m_receivedBytes.Substring(0, pos);
+                m_receivedBytes = m_receivedBytes.Substring(pos + 1);
+
+                pos = command.IndexOf('@');
+                if (pos >= 0)
+                {
+                    command = command.Substring(pos + 1);
+                    if (command[0] == 'L')
+                    {
+                        ParseList(command);
+                    }
+                }
+            }
+        }
+
+        private static void DataReceivedHandler(
+            object sender,
+            SerialDataReceivedEventArgs e)
+        {
+            ArduinoPort port = (ArduinoPort)sender;
+            string data = port.ReadExisting();
+            port.OnDataReceived(data);
+        }
+
+        private void ParseList(string list)
+        {
+            var pinList = list.Split('|');
+            int numStrings = pinList.Length;
+            for (int i = 1; i < numStrings; ++i)
+            {
+                string pinDescription = pinList[i];
+                m_pins.Add(new ArduinoPin(this, pinDescription));
+            }
+            OnPinsAvailable(EventArgs.Empty);
+        }
+
+        protected virtual void OnPinsAvailable(EventArgs e)
+        {
+            if (PinsAvailable != null)
+            {
+                PinsAvailable(this, e);
+            }
+        }
+    }
+}
